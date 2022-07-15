5cd2f4082c68d9acc5f28c0e8dc22dd236933bd2
TaimurHasan <taimurhasan11@gmail.com>
2022-07-15 15:42:23 -0400

    adding indexeddb for offline functionality and cart persistence

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,65 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // open connection to db
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    // create variables to hold reference to database, transaction, and object store
+    let db, tx, store;
+
+    // if version has changed (or if this is the first time) create three object stores
+    request.onupgradeneeded = function(e) {
+      const db =  request.result;
+
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' });
+      db.createObjectStore('cart', { keyPath: '_id' });
+    };
+
+    // error handling
+    request.onerror = function(e) {
+      console.log('There was an error')
+    };
+
+    // on database open success
+    request.onsuccess = function(e) {
+      db = request.result;
+
+      tx = db.transaction(storeName, 'readwrite');
+
+      store = tx.objectStore(storeName);
+
+      db.onerror = function(e) {
+        console.log('error', e);
+      };
+
+      switch (method) {
+        case 'put':
+          store.put(object);
+          resolve(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function () {
+            resolve(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default:
+          console.log('no valid method');
+          break;
+      }
+
+      // close connection after transaction is complete
+      tx.oncomplete = function() {
+        db.close();
+      }
+    }
+
+  })
+}
